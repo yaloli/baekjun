e55c50cadad1c5962756533678c0a76523d9e631
dongwoo <shp05179@gmail.com>
2023-01-22 22:09:38 +0900

    문제풀이: 문제 후기 주석

--- 15591(MooTube).py
+++ 15591(MooTube).py
@@ -1,6 +1,9 @@
-# 시작  9:01
+# 시작  9:01 끝 10:08
 #  다익스트라 알고리즘인듯
 # 아님 dfs임, 이유: 길이 유일함
+
+# dfs로 풀면 메모리초과, bfs로 풀어야 하는 문제, 
+# bfs도 메모리 낭비없이 해시사용해서 풀어야 시간 초과 피할 수 있음
 from collections import deque
 
 def solution():
